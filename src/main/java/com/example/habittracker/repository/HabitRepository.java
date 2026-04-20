37bca721d075dc871b0373175b91d28344d34afd
Yxx <sc22x2y@leeds.ac.uk>
2026-04-20 19:27:39 +0800

    Create a Spring Data JPA repository for the Habit entity.

---
+++ src/main/java/com/example/habittracker/repository/HabitRepository.java
@@ -0,0 +1,7 @@
+package com.example.habittracker.repository;
+
+import com.example.habittracker.model.Habit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HabitRepository extends JpaRepository<Habit, Long> {
+}
